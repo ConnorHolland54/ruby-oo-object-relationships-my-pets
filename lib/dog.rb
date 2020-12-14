4844aaae5e475ffe1322559c29b5958595b05e59
Connor Holland <supersoccergoalie123@gmail.com>
2020-12-14 14:14:02 -0700

    commit tree b87bc233ef144186446ca71434ebc15e96042b0e

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,26 @@
 class Dog
   # code goes here
+
+  attr_accessor :owner, :mood
+  attr_reader :name
+
+  @@all = []
+
+  def initialize(name, owner)
+    @owner = owner
+    @name = name
+    @mood = "nervous"
+    save
+  end
+
+  def self.all
+    @@all
+  end
+
+  def save
+    Dog.all << self
+  end
+
+
+
 end
